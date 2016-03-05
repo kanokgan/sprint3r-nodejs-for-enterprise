5ed45f2fde316110ec5d38f7cd2d70936c0e5e69
Kanokgan Hanmoungtham <kanokgan@gmail.com>
2016-03-05 20:40:01 +0700

    Init Project

---
+++ config.js
@@ -0,0 +1,9 @@
+var config ={};
+config.DB_PROVIDER ="OrientDB";
+config.DB_HOST ="localhost";
+config.DB_PORT=2424;
+config.DB_USER="root";
+config.DB_PASSWORD ="0C617CF91B09F5DE669A5AFE2815DF13F21C8AE495C562845F843208CBDE7A1C";
+config.DB_DATABASE ="SocialNetwork";
+config.API_USERS ="U2FsdGVkX1+/kMQOUqiwpb0UmeQW+F96B2JY/njO49IjPf2Y+8YYlvDSlYqXegJRqEkQraG5NiEL/0YflAfPSyxjbxZhohhHn0a2fscSGGVUkYrrK9e0m5yuKl+wgg==";
+module.exports = config;
